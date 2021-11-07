6cc92d6e04af71061b3b6efa6d3293af2f83d2a3
kavishamodi97 <kavishamodi971@gmail.com>
2021-11-07 23:02:08 +0530

    Create Routing and Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap'
+import Home from './Home'
+import About from './About'
+import Users from './Users';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about" >About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
