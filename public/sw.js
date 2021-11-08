62574407717d05ac5a512eb90ea8283163eb14f9
kavishamodi97 <kavishamodi971@gmail.com>
2021-11-08 15:50:34 +0530

    Offline sync support

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,34 @@
-console.warn("ws file is public folder")
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/static/css/main.chunk.css',
+                '/static/js/vendors~main.chunk.js',
+                '/bootstrap.min.css',
+                '/index.html',
+                '/favicon.ico',
+                '/',
+                "/users"
+            ])
+        })
+    )
+})
+
+
+this.addEventListener("fetch", (event) => {
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+                let requestUrl = event.request.clone();
+                return fetch(requestUrl)
+            })
+        )
+    }
+})
--- src/Users.js
+++ src/Users.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Table } from 'react-bootstrap';
+
 export default function Users() {
+
+    const [data, setData] = useState([]);
+    const [mode, setMode] = useState('online')
+
+    useEffect(() => {
+        let url = "https://jsonplaceholder.typicode.com/users";
+        fetch(url).then((response) => {
+            response.json().then((result) => {
+                console.log("result", result)
+                setData(result)
+                localStorage.setItem("users", JSON.stringify(result))
+            })
+        }).catch(err => {
+            let collection = localStorage.getItem("users")
+            setData(JSON.parse(collection))
+            setMode('offline')
+        })
+    }, [])
+
     return (
         <div>
-            <h1>Users Component</h1>
+            <div>
+                {
+                    mode === 'offline' ? <div>
+                        <div class="alert alert-warning" role="alert">
+                            you are in offline mode or some issue with connection
+                        </div>
+                    </div> : null
+                }
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item) => (
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>{item.address.street}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
         </div>
     )
 }
